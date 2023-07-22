e9dbac5e1ef09f498ba69f1691e3d509bf5afa23
Luca Trautmann <guy_montag@posteo.net>
2023-07-22 14:05:39 +0100

    feat: Completed the Dataloader
    The dataloader for the spectrograms is completed and outputs spec
    trogramms of size B, 2, 512, 512 or B, 2 512, stft(sr * length in
    seconds/ hopsize)

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,8 +1,7 @@
 import torch
-import matplotlib.pyplot as plt
-import IPython.display as ipd
+import librosa
 import os
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 import pandas as pd
 import torchaudio
 import torchshow as ts
@@ -11,33 +10,41 @@ import random
 
 class AvianNatureSounds(Dataset):
     def __init__(self,
-                annotation_file=None,
+                # file args
+                annotation_file_path=None,
                 root_dir='../',
-                mel_spectrogram = None,
-                mode='wav',max_ms=int(1e+6),
                 key='habitat',
+
+                # sound transformation args
+                mode='stft',
+                length=5,
+                sampling_rate=44100,
                 n_fft=1024,
-                hop_length=512):
+                hop_length=512,
+                mel_spectrogram = None,
+                verbose=False,
+                fixed_limit=False
+                ):
         
-
-
-
         self.column = key
-        
-        if self.column != None:
-            try:
-                self.column in ['fileName','habitat']
-            except KeyError:
-                print(f'The available keys are: fileName,habitat')
-
-        self.annotation_file = pd.read_csv(annotation_file).sort_values(self.column)
+        self.annotation_file = pd.read_csv(annotation_file_path).sort_values(self.column)
         self.root_dir = root_dir
         self.mel_transformation = mel_spectrogram
         self.AmplitudeToDB = torchaudio.transforms.AmplitudeToDB()
         self.mode = mode
-        self.max_ms = max_ms
+        self.length = length
         self.n_fft = n_fft
         self.hop_length = hop_length
+        self.sampling_rate = sampling_rate
+        self.verbose = verbose
+        self.fixed_limit = fixed_limit
+        self.return_signal_dims()
+
+    def return_signal_dims(self):
+        if self.verbose:
+            print(f'Returning signal dimensions: H = {self.n_fft // 2 + 1} W = {((self.sampling_rate * self.length)//self.hop_length) + 1}')
+        else:
+            pass
 
 
     def __len__(self):
@@ -45,9 +52,32 @@ class AvianNatureSounds(Dataset):
 
     def __getitem__(self, index):
         ####################################################################################
+        # Get stft spectrograms
+        ####################################################################################
+        if self.mode == 'stft':
+            audio_sample_path = os.path.join(self.root_dir,os.listdir(self.root_dir)[index])
+            label = self.annotation_file.iloc[index][self.column]
+            signal, sr = torchaudio.load(audio_sample_path)
+
+            signal = self.clip(signal, sr, self.length,fixed_limit=self.fixed_limit)
+            stft = torch.stft(signal, n_fft=self.n_fft, hop_length=self.hop_length,win_length=self.n_fft, normalized=False, return_complex=True)
+
+            # librosa_stft = librosa.stft(signal.numpy(), n_fft=self.n_fft, hop_length=self.hop_length)
+            stft = stft[1:][:]
+            mag = self.AmplitudeToDB(torch.abs(stft))
+            phase = torch.angle(stft)
+
+            mag, phase = mag.unsqueeze(0), phase.unsqueeze(0)
+
+
+            return torch.cat([mag,phase],dim=0), mag, label
+
+
+
+        ####################################################################################
         # Get wav files
         ####################################################################################
-        if self.mode == 'wav':
+        elif self.mode == 'wav':
             audio_sample_path = os.path.join(self.root_dir,os.listdir(self.root_dir)[index])
             label = self.annotation_file.iloc[index][self.column]
             signal, sr = torchaudio.load(audio_sample_path)
@@ -67,60 +97,27 @@ class AvianNatureSounds(Dataset):
             return (signal, sr), label     
           
 
-        ####################################################################################
-        # Get stft spectrograms
-        ####################################################################################
-        elif self.mode == 'stft':
-            audio_sample_path = os.path.join(self.root_dir,os.listdir(self.root_dir)[index])
-            label = self.annotation_file.iloc[index][self.column]
-            signal, sr = self.pad_trunc(torchaudio.load(audio_sample_path),max_ms=self.max_ms)
-
-            stft = torch.stft(signal, n_fft=self.n_fft, hop_length=self.hop_length, normalized=True, return_complex=True)
-
-            mag = self.AmplitudeToDB(torch.abs(stft))
-            phase = torch.angle(stft)
-
-            return torch.cat([mag,phase],dim=0), mag, label
-        
-
-        ####################################################################################
-        # Get stft spectrograms
-        ####################################################################################
-        elif self.mode == 'testing':
-            audio_sample_path = os.path.join(self.root_dir,os.listdir(self.root_dir)[index])
-            label = self.annotation_file.iloc[index][self.column]
-            signal, sr = self.pad_trunc(torchaudio.load(audio_sample_path), max_ms=self.max_ms)
-
-            stft = torch.stft(signal, n_fft=256, hop_length=16, normalized=True, return_complex=True)
-            mag = self.AmplitudeToDB(torch.abs(stft))
-            phase = torch.angle(stft)
-
-            return torch.cat([mag,phase],dim=0), label
-
-
-
         else:
             raise NotImplementedError
     
+
     @staticmethod
-    def pad_trunc(aud, max_ms):
-        sig, sr = aud
-        num_rows, sig_len = sig.shape
-        max_len = int(sr//1000 * max_ms)
-        if (sig_len > max_len):
-            # Truncate the signal to the given length
-            sig = sig[:,:max_len]
+    def clip(audio_signal, sr, desired_length,fixed_limit=False):
+        num_rows, sig_len = audio_signal.shape
+        length = int(sr * desired_length)
 
-        elif (sig_len < max_len):
-            # Length of padding to add at the beginning and end of the signal
-            pad_begin_len = random.randint(0, max_len - sig_len)
-            pad_end_len = max_len - sig_len - pad_begin_len
+        if fixed_limit:
+            sig = audio_signal[0][:262100]
+            return  sig
 
-            # Pad with 0s
-            pad_begin = torch.zeros((num_rows, pad_begin_len))
-            pad_end = torch.zeros((num_rows, pad_end_len))
+        elif sig_len > length:
+            offset = random.randint(0, sig_len - length)
+            sig = audio_signal[:, offset:(offset+length)]
 
-            sig = torch.cat((pad_begin, sig, pad_end), 1)
-            
-        return (sig, sr)
+            return sig
+        else:
+            return audio_signal
+
+
+
 
