44579647276ca2a2dd168bc318620543f5290de1
Luca Trautmann <guy_montag@posteo.net>
2023-08-10 13:52:22 +0200

    completed the deep griffin lim Model

--- DGLim/hyperparameter.py
+++ DGLim/hyperparameter.py
@@ -1,10 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Tuple, Dict, Any, Union
 
 @dataclass
 class Hyperparameter:
     # dataloader args
-    annotation_file_path: str = '../../data/AvianID_AcousticIndices/UK_AI.csv'
-    root_dir: str = '../../data/UK_BIRD/'
+    annotation_file_path: str = './data/AvianID_AcousticIndices/UK_AI.csv'
+    root_dir: str = './data/UK_BIRD/'
     key: str = 'habitat'
     mode: str = 'stft'
     length: int = 1
@@ -21,14 +22,21 @@ class Hyperparameter:
     # Model args
     weight_decay: float = 0.0001
     learning_rate: float = 5e-4
-    epochs: int = 100
+    min_lr: float = 5e-8
+    epochs: int = 3
+    scheduler: Any = field(default=None)
+    criterion: Any = field(default=None)
+    optimizer: Any = field(default=None)
 
     # save / load model
-    save_path: str = '../data/checkpoints/DGL.pth.tar'
-    load_path: str = '../data/checkpoints/DGL.pth.tar'
+    save_path: str = './checkpoints/DGL.pth.tar'
+    load_path: str = './checkpoints/DGL.pth.tar'
 
 
-
+    def update_hyperparameter(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 
 hp = Hyperparameter()
+
